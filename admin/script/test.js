c8d3f0316cd675b515731b63b76d78bcfde3dc0e
zloy-ponchik <nikyar852@gmail.com>
2024-12-17 14:41:18 +0300

    Delete admin directory

--- admin/script/test.js
+++
@@ -1,7 +0,0 @@
-fetch('test.json')
-    .then(response => response.json())
-    .then(data => {
-        const element = document.querySelector('pre');
-
-        console.log(JSON.stringify(data, null, '\t'));
-    });
