3018b10505ac60761157c0a28095a218a2244bf2
Quasar777 <58311183+Quasar777@users.noreply.github.com>
2024-12-17 20:47:28 +0300

    Update script.js
    main script update, adding new input type data

--- client/js/script.js
+++ client/js/script.js
@@ -1,6 +1,9 @@
 import { select } from './select.js'
 
-
+const dates = [...document.querySelectorAll('#custom-date')]
+dates.forEach((elem) => {
+    elem.valueAsDate = new Date()
+})
 
 select()
 
